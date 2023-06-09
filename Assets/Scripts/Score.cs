8a7283b0a836f47fc42da6896f6142d3e3906ae3
Sameer Faridi <s.faridi007@gmail.com>
2023-06-09 19:33:19 +0530

    dimaag kharaab ho gayaa

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,94 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor;
-using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-
-public class Score : MonoBehaviour
-{
-    [Header("Players :")]
-    [SerializeField] GameObject player1;
-    [SerializeField] GameObject player2;
-
-    [Header("Goal :")]
-    [SerializeField] GameObject leftGoalWall;
-    [SerializeField] GameObject rightGoalWall;
-
-    [Header("Ball :")]
-    [SerializeField] GameObject ball;
-
-    [Header("Score :")]
-    [SerializeField] TMP_Text player1Score;
-    [SerializeField] TMP_Text player2Score;
-    private int p1score = 0;
-    private int p2score = 0;
-
-    [Header("Game Over :")]
-    [SerializeField] GameObject GameOverPannel;
-
-    [SerializeField] Rigidbody2D rb;
-
-    void Start()
-    {
-        GameOverPannel.SetActive(false);
-    }
-
-    void Update()
-    {
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player1" && ball != null)
-        {
-            p1score++;
-            player1Score.text = p1score.ToString();
-            Debug.Log("Player 1 Score: " + p1score);
-        }
-        if (collision.tag == "Player2" && ball != null)
-        {
-            p2score++;
-            player2Score.text = p2score.ToString();
-            Debug.Log("Player 2 Score: " + p2score);
-        }
-        if (collision.tag == "Ball" && leftGoalWall != null)
-        {
-            GameOverPannel.SetActive(true);
-        }
-        if (collision.tag == "Ball" && rightGoalWall != null)
-        {
-            GameOverPannel.SetActive(true);
-        }
-    }
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 using UnityEngine;
 using TMPro;
 
@@ -121,6 +30,98 @@ public class Score : MonoBehaviour
     {
         GameOverPanel.SetActive(false);
         ballStartPosition = ball.transform.position;
+        Debug.Log("I am start");
+
+    }
+
+    void Update()
+    {
+        Debug.Log("I am working ");
+    }
+    private void GameOver()
+    {
+        Time.timeScale = 0f;
+        GameOverPanel.SetActive(true);
+    }
+
+    public void ResetGame()
+    {
+        Time.timeScale = 1f;
+        p1score = 0;
+        p2score = 0;
+        player1Score.text = "0";
+        player2Score.text = "0";
+        GameOverPanel.SetActive(false);
+
+        if (ball != null)
+        {
+            ball.transform.position = ballStartPosition;
+            ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "player1" && ball != null)
+        {
+            p1score++;
+            player1Score.text = p1score.ToString();
+            Debug.Log("Player 1 Score: " + p1score);
+        }
+
+        /*if (collision.collider.tag == "Player2" && ball != null)
+        {
+            p2score++;
+            player2Score.text = p2score.ToString();
+            Debug.Log("Player 2 Score: " + p2score);
+        }
+        if (collision.collider.tag == "leftgole")
+        {
+            Debug.Log("colide");
+
+            GameOverPanel.SetActive(true);
+        }
+        if (collision.collider.tag == "Ball" && rightGoalWall != null)
+        {
+            Debug.Log("colide galat chez");
+            GameOverPanel.SetActive(true);
+        }*/
+    }
+}
+
+
+
+
+/*
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [Header("Players :")]
+    [SerializeField] GameObject player1;
+    [SerializeField] GameObject player2;
+
+    [Header("Goal :")]
+    [SerializeField] GameObject leftGoalWall;
+    [SerializeField] GameObject rightGoalWall;
+
+    [Header("Ball :")]
+    [SerializeField] GameObject ball;
+    private Vector3 ballStartPosition;
+
+    [Header("Score :")]
+    [SerializeField] TMP_Text player1Score;
+    [SerializeField] TMP_Text player2Score;
+    private int p1score = 0;
+    private int p2score = 0;
+
+    [Header("Game Over :")]
+    [SerializeField] GameObject GameOverPanel;
+
+    void Start()
+    {
+        GameOverPanel.SetActive(false);
+        ballStartPosition = ball.transform.position;
     }
 
     void Update()
@@ -128,31 +129,34 @@ public class Score : MonoBehaviour
 
     }
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.tag == "Player1" && ball != null)
+        if (collision.collider.tag == "Player1" && ball != null)
         {
             p1score++;
-            player1Score.text = p1score.ToString();
             Debug.Log("Player 1 Score: " + p1score);
+            player1Score.text = p1score.ToString();
         }
-        if (collision.tag == "Player2" && ball != null)
+
+        if (collision.collider.tag == "player2" && ball != null)
         {
             p2score++;
-            player2Score.text = p2score.ToString();
             Debug.Log("Player 2 Score: " + p2score);
+            player2Score.text = p2score.ToString();
         }
-        if (collision.tag == "Ball" && leftGoalWall != null)
+
+        if (collision.collider.tag == "leftGoal")
         {
-            GameOverPanel.SetActive(true);
+            Debug.Log("Collision with left goal");
+            GameOver();
         }
-        if (collision.tag == "Ball" && rightGoalWall != null)
+
+        if (collision.collider.tag == "rightGoal")
         {
-            GameOverPanel.SetActive(true);
+            Debug.Log("Collision with right goal");
+            GameOver();
         }
     }
-  
 
     private void GameOver()
     {
@@ -175,93 +179,4 @@ public class Score : MonoBehaviour
             ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* private void OnTriggerEnter2D(Collider2D collision)
-   {
-       if (collision.CompareTag("Player1") && ball != null)
-       {
-           p1score++;
-           player1Score.text = p1score.ToString();
-           Debug.Log("Player 1 Score: " + p1score);
-       }
-       if (collision.CompareTag("Player2") && ball != null)
-       {
-           p2score++;
-           player2Score.text = p2score.ToString();
-           Debug.Log("Player 2 Score: " + p2score);
-       }
-       if (collision.CompareTag("Ball") && leftGoalWall != null)
-       {
-           GameOver();
-       }
-       if (collision.CompareTag("Ball") && rightGoalWall != null)
-       {
-           GameOver();
-       }
-   }*/
+}*/
